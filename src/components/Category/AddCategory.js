58ea5b973819817cd197c5e7fe578f9719d287f7
hamrosh <hamroshroy@gmail.com>
2018-06-03 19:33:05 +0530

    Second Commit- with grid

--- src/components/Category/AddCategory.js
+++ src/components/Category/AddCategory.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 // import for the mutation connection of react and graphql
-import { Mutation } from 'react-apollo';
+import { Mutation, Query } from 'react-apollo';
 // import custom defined mutations
 import { ADD_CATEGORY } from './gql/Mutations';
+// import custom defined queries
+import { GET_CATEGORY_LIST } from './gql/Queries';
 // import for the form handling api formik which helps us in creating forms in react in a simple way
+
+// import for grid view raeact-table
+import ReactTable from 'react-table';
+//import for react - table css
+import 'react-table/react-table.css';
 
 import { Formik, Form, Field } from 'formik';
 const Yup = require('yup');
@@ -13,49 +20,51 @@ class AddCategory extends Component {
     return (
       <React.Fragment>
         {/* Declare Mutation Tag , pass the mutation as function and the mutation result */}
-        <Mutation
-          mutation={ADD_CATEGORY}
-          errorPolicy="all"
-          onError={e => console.log(e.message)}
-          onCompleted={data => console.log(data)}
-        >
-          {(AddCategory, { data, loading, error }) => {
-            console.log('Hello');
-            return (
-              <div class="container">
-                <Formik
-                  validationSchema={Yup.object().shape({
-                    category: Yup.string()
-                      .min(2, 'minimum 2 characters long')
-                      .required('Required!')
-                  })}
-                  onSubmit={(values, actions) => {
-                    actions.setSubmitting(true);
-                    AddCategory({
-                      variables: {
-                        input: {
-                          category: values.category,
-                          createdby: values.createdby
-                        }
-                      }
-                    });
-                    actions.resetForm({});
-                    actions.setSubmitting(false);
-                  }}
-                  initialValues={{
-                    category: '',
-                    createdby: ''
-                  }}
-                  render={({
-                    errors,
-                    touched,
-                    isSubmitting,
-                    dirty,
-                    values
-                  }) => (
-                    <div className="container">
-                      <div className="row">
-                        <div className="col">
+
+        <div className="container">
+          <div className="row">
+            <div class="col-6">
+              {' '}
+              <Mutation
+                mutation={ADD_CATEGORY}
+                errorPolicy="all"
+                onError={e => console.log(e.message)}
+                onCompleted={data => console.log(data)}
+                refetchQueries={['AllCategories']}
+              >
+                {(AddCategory, { data, loading, error }) => {
+                  return (
+                    <div>
+                      <Formik
+                        validationSchema={Yup.object().shape({
+                          category: Yup.string()
+                            .min(2, 'minimum 2 characters long')
+                            .required('Required!')
+                        })}
+                        onSubmit={(values, actions) => {
+                          actions.setSubmitting(true);
+                          AddCategory({
+                            variables: {
+                              input: {
+                                category: values.category,
+                                createdby: values.createdby
+                              }
+                            }
+                          });
+                          actions.resetForm({});
+                          actions.setSubmitting(false);
+                        }}
+                        initialValues={{
+                          category: '',
+                          createdby: ''
+                        }}
+                        render={({
+                          errors,
+                          touched,
+                          isSubmitting,
+                          dirty,
+                          values
+                        }) => (
                           <Form>
                             <div className="form-group">
                               <label htmlFor="category">Category</label>
@@ -103,31 +112,80 @@ class AddCategory extends Component {
                               Submit
                             </button>
                           </Form>
-                          <br />
+                        )}
+                      />
+                      {data && (
+                        <div class="alert alert-success" role="alert">
+                          <p>Saved Category</p>
                         </div>
-                      </div>
+                      )}
+                      {loading && (
+                        <div class="alert alert-warning" role="alert">
+                          <p>Saving Category</p>
+                        </div>
+                      )}
+                      {error && (
+                        <div class="alert alert-danger" role="alert">
+                          <p> Error{error.message} :( Please try again</p>
+                        </div>
+                      )}
                     </div>
-                  )}
-                />
-                {data && (
-                  <div class="alert alert-success" role="alert">
-                    <p>Saved Category</p>
-                  </div>
-                )}
-                {loading && (
-                  <div class="alert alert-warning" role="alert">
-                    <p>Saving Category</p>
-                  </div>
-                )}
-                {error && (
-                  <div class="alert alert-danger" role="alert">
-                    <p> Error{error.message} :( Please try again</p>
-                  </div>
-                )}
-              </div>
-            );
-          }}
-        </Mutation>
+                  );
+                }}
+              </Mutation>
+            </div>
+            <div className="col-6">
+              <Query query={GET_CATEGORY_LIST}>
+                {({ loading, error, data }) => {
+                  if (loading) return 'Loading...';
+                  if (error) return `Error! ${error.message}`;
+                  {
+                    console.log(data);
+                  }
+                  return (
+                    <div>
+                      <ReactTable
+                        data={data.allCategories}
+                        columns={[
+                          {
+                            Header: 'Categories',
+                            columns: [
+                              {
+                                Header: 'Category',
+                                accessor: 'category'
+                              },
+                              {
+                                Header: 'Created By',
+                                accessor: 'createdby'
+                              },
+                              {
+                                id: 'button',
+                                accessor: 'category',
+                                Cell: ({ value }) => (
+                                  <button
+                                    className="btn btn-primary"
+                                    onClick={() => {
+                                      console.log('clicked value', value);
+                                    }}
+                                  >
+                                    Delete
+                                  </button>
+                                )
+                              }
+                            ]
+                          }
+                        ]}
+                        defaultPageSize={10}
+                        className="-striped -highlight"
+                      />
+                      <br />
+                    </div>
+                  );
+                }}
+              </Query>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
--- src/components/Category/gql/Queries.js
+++ src/components/Category/gql/Queries.js
@@ -1,9 +1,8 @@
 import gql from 'graphql-tag';
 // create the query from string as graphql query using gql tag import
-export const GET_QUESTION_LIST = gql`
-  {
+export const GET_CATEGORY_LIST = gql`
+  query AllCategories {
     allCategories {
-      id
       category
       createdby
     }
